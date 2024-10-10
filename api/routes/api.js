562140c3ee6e831e1ee087276f87b073ca50175e
JinBo3619 <145665315+JinBo3619@users.noreply.github.com>
2024-10-10 16:52:30 +0800

    add new api

--- api/routes/api.js
+++ api/routes/api.js
@@ -17,7 +17,7 @@ router.get('/categories', (req, res) => {
 });
 
 
-// 
+//
 router.get('/fundraiser/:id', (req, res) => {
   const { id } = req.params;
   db.query('SELECT a.*,b.`NAME`AS CATEGORY_NAME FROM FUNDRAISER a LEFT JOIN CATEGORY b ON a.CATEGORY_ID = b.CATEGORY_ID WHERE a.ACTIVE = 1 AND a.FUNDRAISER_ID = ?', [id], (err, results) => {
@@ -40,6 +40,31 @@ router.get("/search", async (req, res) => {
   }
   db.query(`SELECT a.*,b.NAME AS CATEGORY_NAME FROM FUNDRAISER a 
     LEFT JOIN CATEGORY b ON a.CATEGORY_ID = b.CATEGORY_ID WHERE 1=1 ` + sql, (err, results) => {
+    if (err) return res.status(500).json(err);
+    res.json(results);
+  });
+});
+
+
+router.get("/fundraiser/:id/donations", async (req, res) => {
+  const { id } = req.params;
+  db.query('SELECT * FROM DONATION WHERE FUNDRAISER_ID = ?', [id], (err, results) => {
+    if (err) return res.status(500).json(err);
+    res.json(results);
+  });
+});
+
+router.post("/fundraiser/donations", async (req, res) => {
+  const { giver, amount, fundraiseId } = req.body;
+  db.query('INSERT INTO DONATION(DATE, AMOUNT, GIVER, FUNDRAISER_ID) VALUES(?,?,?,?)', [new Date(), amount, giver, fundraiseId], (err, results) => {
+    if (err) return res.status(500).json(err);
+    res.json(results);
+  });
+});
+
+router.post("/fundraiser", async (req, res) => {
+  const { organizer, caption, targetFunding, currentFunding, city, active, categoryId } = req.body;
+  db.query('INSERT INTO FUNDRAISER(ORGANIZER, CAPTION, TARGET_FUNDING, CURRENT_FUNDING, CITY, ACTIVE, CATEGORY_ID) VALUES(?,?,?,?,?,?,?)', [organizer, caption, targetFunding, currentFunding, city, active, categoryId], (err, results) => {
     if (err) return res.status(500).json(err);
     res.json(results);
   });
